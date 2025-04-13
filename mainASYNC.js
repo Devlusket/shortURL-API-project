e7a496c36c6f9b4fd3308a2e1dd09321dc6235fc
Lucas Soares <tsuunakage@gmail.com>
2025-04-13 18:23:51 -0300

    first commit

---
+++ mainASYNC.js
@@ -0,0 +1,44 @@
+// information to reach API
+const apiKey = 'dd9fa37fe411456d92074e9b0f518442';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// Asynchronous functions
+const shortenUrl = async () => {
+	const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  try {
+    fetch(url, {
+      method: 'POST',
+      body: data,
+      headers: {
+        'Content-type': 'application/json',
+        'apikey': apiKey
+      }
+    })
+    const response = await fetch(
+      // add code here
+    );
+		if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Clear page and call Asynchronous functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
